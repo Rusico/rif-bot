68c78b01bfaa5536a70c5a0967ea44b060ef544a
rusico <seniorroot@gmail.com>
2019-03-17 15:42:46 +0300

    dfd

--- src/main/java/apibank/club/bot/BotApplication.java
+++ src/main/java/apibank/club/bot/BotApplication.java
@@ -1,11 +1,9 @@
 package apibank.club.bot;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
@@ -13,18 +11,13 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 @SpringBootApplication
 public class BotApplication {
 
-  //@Value("${telegram.proxy.set}")
-  private static boolean proxy = false;
-
-  //@Value("${telegram.proxy.host}")
+  private static boolean proxy = true;
   private static String proxyHost = "149.56.27.45";
-
-  //@Value("${telegram.proxy.port}")
   private static int proxyPort = 1080;
 
   public static void main(String[] args) {
     ApiContextInitializer.init();
-    SpringApplication.run(BotApplication.class, args);
+    //SpringApplication.run(BotApplication.class, args);
 
     TelegramBotsApi botsApi = new TelegramBotsApi();
     try {
