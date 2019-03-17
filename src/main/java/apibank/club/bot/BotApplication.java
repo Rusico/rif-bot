9c312403bfcfa28a8b8d37f8533c6446a7a65a69
Ilya <mra4niykeks@gmail.com>
2019-03-17 20:17:37 +0300

    Запустил без вебхука. Тестовый эхо-сервис

--- src/main/java/apibank/club/bot/BotApplication.java
+++ src/main/java/apibank/club/bot/BotApplication.java
@@ -11,30 +11,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 @SpringBootApplication
 public class BotApplication {
 
-  private static boolean proxy = false;
-  private static String proxyHost = "149.56.27.45";
-  private static int proxyPort = 1080;
-
   public static void main(String[] args) {
-    ApiContextInitializer.init();
     SpringApplication.run(BotApplication.class, args);
-
-    try {
-      TelegramBotsApi botsApi = new TelegramBotsApi(
-              "src/main/resources/ssl/bot.jks",
-              "janjakrusso",
-              "https://rif-bot.herokuapp.com",
-              "https://rif-bot.herokuapp.com");
-
-      DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
-      if(proxy) {
-        botOptions.setProxyHost(proxyHost);
-        botOptions.setProxyPort(proxyPort);
-        botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
-      }
-      botsApi.registerBot(new Bot(botOptions));
-    } catch (TelegramApiRequestException e) {
-      e.printStackTrace();
-    }
   }
 }
