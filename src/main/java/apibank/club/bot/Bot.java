17d8967fcdef35854369d87761913be2ef374b4d
rusico <seniorroot@gmail.com>
2019-03-17 19:37:57 +0300

    webhook настроил

--- src/main/java/apibank/club/bot/Bot.java
+++ src/main/java/apibank/club/bot/Bot.java
@@ -4,15 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-public class Bot extends TelegramLongPollingBot {
+public class Bot extends TelegramWebhookBot {
   private static Logger logger = LoggerFactory.getLogger(Bot.class);
 
   private String token = "833516765:AAEqVBbecQ1gYgJb8TcfGxPrOSjrjdj_DB8";
@@ -21,8 +23,13 @@ public class Bot extends TelegramLongPollingBot {
     super(options);
   }
 
-  public void onUpdateReceived(Update update) {
+  @Override
+  public String getBotToken() {
+    return token;
+  }
 
+  @Override
+  public BotApiMethod onWebhookUpdateReceived(Update update) {
     logger.info("#################### TRY GET MESSAGE #######################################");
 
     try {
@@ -42,13 +49,16 @@ public class Bot extends TelegramLongPollingBot {
     } catch (TelegramApiException e) {
       logger.error(e.getMessage(), e);
     }
+    return null;
   }
 
+  @Override
   public String getBotUsername() {
     return "RusicoBot";
   }
 
-  public String getBotToken() {
-    return token;
+  @Override
+  public String getBotPath() {
+    return "RifBot";
   }
 }
