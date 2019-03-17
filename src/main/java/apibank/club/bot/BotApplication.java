c46fcd6f259f81e88617d76c84fef88dfb1ee489
rusico <seniorroot@gmail.com>
2019-03-17 19:06:10 +0300

    webhook настроил

--- src/main/java/apibank/club/bot/BotApplication.java
+++ src/main/java/apibank/club/bot/BotApplication.java
@@ -1,6 +1,5 @@
 package apibank.club.bot;
 
-import com.sun.deploy.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
